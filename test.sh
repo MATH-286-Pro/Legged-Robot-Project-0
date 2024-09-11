d0c5c97ef0966336eb3e4240f2507d921140f620
MATH-286-Pro <72606969+MATH-286-Pro@users.noreply.github.com>
2024-09-11 22:12:18 +0200

    Delete test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-clear
-
-export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6
-
-# python practical1.py
-python practical2_jacobian.py
-# python practical3_ik.py
-# python practical4_hopping.py
